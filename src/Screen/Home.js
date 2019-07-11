b39dcd6de4235b13fb10fdcc79345543d0806544
이현서 <hyeonseo@ihyeonseoui-MacBook-Pro.local>
2019-07-11 20:46:07 +0900

    hometootherpage

--- src/Screen/Home.js
+++ src/Screen/Home.js
@@ -1,4 +1,7 @@
 import React, { Component } from "react";
+import Postbox from "./Postbox";
+import Send from "./Send";
+import Mypage from "./Mypage";
 import {
   Text,
   Container,
@@ -44,6 +47,8 @@ export default class Home extends Component {
   constructor(props) {
     super(props);
     this.state = {
+      pageto: 1,
+      // 1 = home , 2 = postbox , 3 = mypage , 4 = 편지쓰기
       centerText: "HELLO OWLS!",
       subText: "당신의 펜팔친구를 찾아보세요!",
       matchStatus: "매칭시작",
@@ -51,7 +56,9 @@ export default class Home extends Component {
       // 매칭 버튼을 누르면 서버에 post 요청 -> db에 partnerId가 null인
       // 상대와 서로를 추가한다.
       // 포스트 요청에 따라 푸시요청??
-      matchComplete: true
+      partner: "Ironman",
+      matchComplete: true,
+      postStatus: true
       // 상대가 있으면 true , default = false -> true 면 또 변경.
     };
   }
@@ -63,9 +70,25 @@ export default class Home extends Component {
       });
     }
   }
-
+  ßß;
   render() {
-    const { centerText, subText, matchStatus, matchComplete } = this.state;
+    const {
+      pageto,
+      centerText,
+      subText,
+      matchStatus,
+      matchComplete,
+      partner
+    } = this.state;
+
+    if (pageto === 2) {
+      return <Postbox />;
+    } else if (pageto === 3) {
+      return <Mypage />;
+    } else if (pageto === 4) {
+      return <Send />;
+    }
+
     return (
       <Container>
         <Header style={styles.toplogo}>
@@ -76,7 +99,7 @@ export default class Home extends Component {
           {matchComplete === false ? (
             <Text style={styles.maintext}>{centerText}</Text>
           ) : (
-            <Text style={styles.maintext}>My penpal : 'NickName'!</Text>
+            <Text style={styles.maintext}>My penpal : '{partner}'!</Text>
           )}
 
           {matchComplete === false ? (
@@ -88,7 +111,14 @@ export default class Home extends Component {
 
         <Footer>
           <FooterTab>
-            <Button style={styles.footer}>
+            <Button
+              style={styles.footer}
+              onPress={() => {
+                this.setState({
+                  pageto: 2
+                });
+              }}
+            >
               <Text>편지함</Text>
             </Button>
             <Button
@@ -100,12 +130,23 @@ export default class Home extends Component {
                     subText: "펜팔친구를 찾고 있어요!",
                     matchStatus: "매칭 중"
                   });
+                } else {
+                  this.setState({
+                    pageto: 4
+                  });
                 }
               }}
             >
               <Text>{matchStatus}</Text>
             </Button>
-            <Button style={styles.footer}>
+            <Button
+              style={styles.footer}
+              onPress={() => {
+                this.setState({
+                  pageto: 3
+                });
+              }}
+            >
               <Text>마이페이지</Text>
             </Button>
           </FooterTab>
