0f428145c5b12b25de3aa1aac87efe7911acc918
Tae hoon Kim <xogns656@naver.com>
2019-07-11 16:58:31 +0900

    버튼 생성완료
      -email , password input 생성
      -로그인, 회원가입 버튼 생성

--- src/Screen/SignIn.js
+++ src/Screen/SignIn.js
@@ -1,19 +1,32 @@
 import React, { Component } from "react";
-import { Container, Content, Text, Body, Input, Button } from "native-base";
+import {
+  Container,
+  Content,
+  Text,
+  Body,
+  Input,
+  Button,
+  Bottom
+} from "native-base";
 
 export default class SignIn extends Component {
   render() {
     return (
       <Container>
         <Body />
+        <Text>당신의 편지를 배달해드립니다</Text>
         <Text>owlPost</Text>
         <Content>
           <Input placeholder="e-mail" />
           <Input placeholder="password" />
           <Button>
-            <Text>dsfds</Text>
+            <Text>Sign In</Text>
           </Button>
         </Content>
+
+        <Button>
+          <Text>Sign up</Text>
+        </Button>
       </Container>
     );
   }
