cb4d8d0d9902fa91ea1b1688398fceb7cd5345cb
Tae hoon Kim <xogns656@naver.com>
2019-07-11 14:34:40 +0900

    change for error

--- App.js
+++ App.js
@@ -1,6 +1,6 @@
 import React, { Component } from "react";
-import { View } from "react-native";
-``;
+import { StyleSheet, View } from "react-native";
+
 export default class App extends Component {
   constructor(props) {
     super(props);
