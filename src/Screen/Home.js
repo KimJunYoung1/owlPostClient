f9b370addf587378548a2ec36628d65a3f3e5f65
이현서 <hyeonseo@ihyeonseoui-MacBook-Pro.local>
2019-07-11 19:22:57 +0900

    Home-matching-matchfinish

--- src/Screen/Home.js
+++ src/Screen/Home.js
@@ -46,18 +46,18 @@ export default class Home extends Component {
     this.state = {
       centerText: "HELLO OWLS!",
       subText: "당신의 펜팔친구를 찾아보세요!",
-      matchStatus: "매칭시작"
+      matchStatus: "매칭시작",
       // 매칭 버튼을 누르면 메인 텍스트와 서브 텍스트를 변경한다.
+      // 매칭 버튼을 누르면 서버에 post 요청 -> db에 partnerId가 null인
+      // 상대와 서로를 추가한다.
       // 포스트 요청에 따라 푸시요청??
+      matchComplete: true
+      // 상대가 있으면 true , default = false -> true 면 또 변경.
     };
   }
 
-  // onPressMatch() {
-  //   ;
-  // }
-
   render() {
-    const { centerText, subText, matchStatus } = this.state;
+    const { centerText, subText, matchStatus, matchComplete } = this.state;
     return (
       <Container>
         <Header style={styles.toplogo}>
@@ -65,8 +65,17 @@ export default class Home extends Component {
         </Header>
 
         <Container>
-          <Text style={styles.maintext}>{centerText}</Text>
-          <Text style={styles.subtext}>{subText}</Text>
+          {matchComplete === false ? (
+            <Text style={styles.maintext}>{centerText}</Text>
+          ) : (
+            <Text style={styles.maintext}>My penpal : 'NickName'!</Text>
+          )}
+
+          {matchComplete === false ? (
+            <Text style={styles.subtext}>{subText}</Text>
+          ) : (
+            <Text style={styles.subtext}>하루 한 통, 마음을 전해보세요.</Text>
+          )}
         </Container>
 
         <Footer>
@@ -77,11 +86,13 @@ export default class Home extends Component {
             <Button
               style={styles.footer}
               onPress={() => {
-                this.setState({
-                  centerText: "FIND FRIEND!",
-                  subText: "펜팔친구를 찾고 있어요!",
-                  matchStatus: "매칭 중"
-                });
+                if (matchComplete === false) {
+                  this.setState({
+                    centerText: "FIND FRIEND!",
+                    subText: "펜팔친구를 찾고 있어요!",
+                    matchStatus: "매칭 중"
+                  });
+                }
               }}
             >
               <Text>{matchStatus}</Text>
