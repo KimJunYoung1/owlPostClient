e52a39077bfad442f1c445b18f6b0a12a1e9d1af
이현서 <hyeonseo@ihyeonseoui-MacBook-Pro.local>
2019-07-15 16:42:08 +0900

    fetchFn-ing

--- src/Screen/Home.js
+++ src/Screen/Home.js
@@ -56,12 +56,12 @@ export default class Home extends Component {
       // 매칭 버튼을 누르면 서버에 post 요청 -> db에 partnerId가 null인
       // 상대와 서로를 추가한다.
       // 포스트 요청에 따라 푸시요청??
-      partner: "Ironman", //  현재 fake값 , null에서 요청으로 받는 값으로 쓸 예정
-      matchComplete: true,
+      partner: " ", //  현재 fake값 , null에서 요청으로 받는 값으로 쓸 예정
+      matchComplete: false,
       // fetch res.partnerNickName 이 null ? true : false
       // 매칭완료이면 true , 매칭 전, 대기 중에는 false
       // 매칭버튼 변경 . 기본 텍스트에서 상대방 닉네임으로 변경
-      postStatus: true,
+      postStatus: false,
       // fetch res.date ? null ? true : false
       // 상대가 편지를 보냈으면 true , default = false -> true 면 또 변경.
       arriveTime: "07/15/19   21:00", //  현재 fake값 , null에서 요청으로 받는 값으로 쓸 예정
@@ -80,47 +80,38 @@ export default class Home extends Component {
   };
 
   componentDidMount() {
-    // let AllUserInfo = "3.15.161.138:5000/user/signin";
-    // fetch(AllUserInfo)
-    //   .then(res => res.json())
-    //   .then(res => {
-    //     if (res.result.partner_nickname === null) {
-    //       this.setState({
-    //         matchComplete: false
-    //       });
-    //     } else {
-    //       this.setState({
-    //         matchComplete: true
-    //       });
-    //     }
-    //     // 또 then? 비동기 ??
-    //     if (this.state.matchComplete === true) {
-    //       // 상대방과 매칭 성공한다면
-    //       // fetch(LOGIN_API)
-    //       //   .then(res => res.json())
-    //       //   .then(res => {
-    //       this.setState({
-    //         partner: res.result.partner_nickname,
-    //         matchStatus: "편지 쓰기"
-    //       });
-    //     } else {
-    //       // 상대방과 매칭 이전이라면 /매칭시작을 누른 적이 있는 지 , 없는 지
-    //       if (this.state.matchStatus === "매칭 중") {
-    //         this.setState({
-    //           matchStatus: "매칭 중"
-    //         });
-    //       } else {
-    //         this.setState({
-    //           matchStatus: "매칭시작"
-    //         });
-    //       }
-    //     }
-    //   });
+    let AllUserInfo = "http://3.15.161.138:5000/user/signin";
+    fetch(AllUserInfo)
+      .then(res => res.json())
+      .then(res => {
+        console.log(res);
+
+        if (res.partner_nickname === null) {
+          this.setState({
+            matchComplete: false
+          });
+          if (this.state.matchStatus === "매칭 중") {
+            this.setState({
+              matchStatus: "매칭 중"
+            });
+          } else {
+            this.setState({
+              matchStatus: "매칭시작"
+            });
+          }
+        } else {
+          this.setState({
+            matchComplete: true,
+            partner: res.partner_nickname,
+            matchStatus: "편지 쓰기"
+          });
+        }
+      });
 
     // fetch(Letter)
     //     .then(res => res.json())
     //     .then(res => {
-    //       if(편지창에 변화가 있다면?){
+    //       if(편지창에 변화가 있다면?---> 이 조건을 어케하지??){
     //         this.setState({
     //           postStatus : true
     //         });
